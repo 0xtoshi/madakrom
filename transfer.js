02617609cac280f9641ede1babdf89c086df3aff
0xToshi <jsonpayment@gmail.com>
2024-02-11 23:50:03 +0700

    Update transfer.js

--- transfer.js
+++ transfer.js
@@ -4,24 +4,26 @@ import { pickRandom } from "./helper/randomize.js";
 
 const accountList = fs.readFileSync("./account.txt", "utf-8").split("\n");
 
-let random = pickRandom(accountList, 20);
+while (true) {
+  await new Promise((r) => setTimeout(r, 3000));
+  let random = pickRandom(accountList, 20);
+  var accountArray = random.filter(function (el) {
+    return el != "" && el !== undefined;
+  });
 
-var accountArray = random.filter(function (el) {
-  return el != "" && el !== undefined;
-});
+  let tx = [];
+  for (let list of accountArray) {
+    let signer = list.split(",");
+    let address = signer[0];
+    let privateKey = signer[1];
 
-let tx = [];
-for (let list of accountArray) {
-  let signer = list.split(",");
-  let address = signer[0];
-  let privateKey = signer[1];
+    tx.push(TransferWithSigner(address, privateKey));
+  }
 
-  tx.push(TransferWithSigner(address, privateKey));
-}
-
-try {
-  let data = await Promise.all(tx);
-  console.log(data);
-} catch (err) {
-  console.log(`Error Boskuuuu`);
+  try {
+    let data = await Promise.all(tx);
+    console.log(data);
+  } catch (err) {
+    console.log(`Error Boskuuuu`);
+  }
 }
